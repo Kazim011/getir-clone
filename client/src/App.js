b35a7a89cf275dc8595d8797ff883bd5cdc81b9b
Put your Name in Here <93521133+sirzatbilen@users.noreply.github.com>
2023-04-26 10:42:59 +0300

    deneme1

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Kazım bir şey bilmiyor</div>;
+  return <div className="App">Kazım AGA bilmiyor</div>;
 }
 
 export default App;
