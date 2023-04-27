b2c0671465d64dbc0016e18c86d8be8f9c96bb61
kazım <kazimnergiz123@gmail.com>
2023-04-27 23:53:40 +0300

    passwordguncel

--- server/data/seeds/001-admin.js
+++ server/data/seeds/001-admin.js
@@ -6,8 +6,17 @@ exports.seed = async function (knex) {
   // Deletes ALL existing entries
   await knex("admin").truncate();
   await knex("admin").insert([
-    { email: "baris@gmail.com", password: "1234" },
-    { email: "sirzatnergiz@gmail.com", password: "1234" },
-    { email: "kazimnergiz123@gmail.com", password: "1234" },
+    {
+      email: "baris@gmail.com",
+      password: "$2a$12$3H9UKvMXcUkA5w39TSE6X.kc8F956ThRi0O.RMn0zsZ9M4SI3z0sS",
+    },
+    {
+      email: "sirzatnergiz@gmail.com",
+      password: "$2a$12$3H9UKvMXcUkA5w39TSE6X.kc8F956ThRi0O.RMn0zsZ9M4SI3z0sS",
+    },
+    {
+      email: "kazimnergiz123@gmail.com",
+      password: "$2a$12$3H9UKvMXcUkA5w39TSE6X.kc8F956ThRi0O.RMn0zsZ9M4SI3z0sS",
+    },
   ]);
 };
